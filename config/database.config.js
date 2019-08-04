91e73d762ccb76025bdbe61c0a78b495656f4bb4
Yuji <yuji.shima09@gmail.com>
2019-08-04 18:44:12 -0300

    db live

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/local'
+    //url: 'mongodb://localhost:27017/local'
+    url: 'mongodb://listup:lua3571113@ds161742.mlab.com:61742/heroku_31sqwc9g'
 }
